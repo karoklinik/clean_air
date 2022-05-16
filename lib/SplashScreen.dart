9847a5fbde3f5c4b13ff5b917de32a8927c18b4c
Karolina Klinik <klinik.karolina@gmail.com>
2022-05-16 18:25:56 +0200

    Download data from location

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -107,15 +107,27 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void executeOnceAfterBuild() async {
+    Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.lowest,
+            forceAndroidLocationManager: true,
+            timeLimit: Duration(seconds: 4))
+        .then((value) => loadLocationData(value))
+        .onError((error, stackTrace) => {
+              Geolocator.getLastKnownPosition(forceAndroidLocationManager: true)
+                  .then((value) => {loadLocationData(value!)})
+            });
+  }
+
+  loadLocationData(Position value) async {
+    var lat = value.latitude;
+    var lng = value.longitude;
+    log(lat.toString() + "x" + lng.toString());
+
     WeatherFactory wf = WeatherFactory("3a68f67fd0d2a30c73a4b81306a7657e",
         language: Language.POLISH);
-    Weather w = await wf.currentWeatherByCityName("Wałbrzych");
+    Weather w = await wf.currentWeatherByLocation(lat, lng);
     log(w.toJson().toString());
 
-    var lat = 50.729022;
-    var lng = 16.272669;
-
-    ///feed/geo::lat;:lng/?token=:token
     String _endpoint = 'http://api.waqi.info/feed';
     var keyword = 'geo:$lat;$lng';
     var key = "3f6502b117e6d1b1d780120c32cfd5c1b75f466b";
@@ -146,8 +158,8 @@ class AirQuality {
 
   AirQuality(Map<String, dynamic> jsonBody) {
     aqi = int.tryParse(jsonBody['data']['aqi'].toString());
-    //pm25 = int.tryParse(jsonBody["data"]["iaqi"]["pm25"]["v"]toString()); -missing in data download from AirAPI
-    pm10 = int.tryParse(jsonBody["data"]["iaqi"]["pm10"]["v"].toString());
+    pm25 = int.tryParse(jsonBody["data"]["iaqi"]["pm25"]["v"].toString());
+    //pm10 = int.tryParse(jsonBody["data"]["iaqi"]["pm10"]["v"].toString());-missing in data download from AirAPI for SanFrancisco, if statement needed
     station = jsonBody['data']['city']['name'].toString();
     setupLevel(aqi!);
   }
